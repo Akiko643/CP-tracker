fbeb7c95eeca632721cbc672dbf73e62c852f917
Akiko <amarjargal.a@steppelink.mn>
2023-04-16 18:37:24 +0800

    gg

--- src/middleware.js
+++ src/middleware.js
@@ -2,10 +2,17 @@ import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    return NextResponse.redirect(new URL("/about-2", request.url));
+    let token = request.cookies.get("token");
+    request.cookies.set("token", "123");
+
+    console.log("token:", token);
+    // const response = NextResponse.next();
+    request.cookies.set({ name: "bobo", value: "wtf" });
+    // const cookie = request.cookies.get("bobo");
+    // console.log(cookie);
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: "/about/:path*",
+    matcher: "/api/:path*",
 };
