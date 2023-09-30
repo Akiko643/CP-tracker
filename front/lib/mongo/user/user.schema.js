7ae660fee949ba523f8d33cdd2c8260c1294f2fe
Amarjargal Ayurzana <akiko@Amarjargals-MacBook-Pro.local>
2023-09-30 00:11:38 -0700

    .

--- front/lib/mongo/user/user.schema.js
+++ front/lib/mongo/user/user.schema.js
@@ -1,14 +1,14 @@
 import { Schema, model, models } from "mongoose";
 
 export const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-        },
-        passwordHash: String,
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    passwordHash: String,
+  },
+  { timestamps: true }
 );
 
 export const User = models.User || model("User", UserSchema);
