48e398b984d9ca1b5e970248163ae1d653909433
Amarjargal Ayurzana <akiko@Amarjargals-MacBook-Pro.local>
2023-12-28 17:28:22 -0800

    remove comment

--- front/app/components/Problem.tsx
+++ front/app/components/Problem.tsx
@@ -94,12 +94,6 @@ const Notes = () => {
 };
 
 const Problem = (problem: { title: string; tags: string[] }) => {
-  // const problem = {
-  //   title: "4A. Watermelon",
-  //   tags: ["brute force", "math", "*800"],
-  //   id: "2",
-  // };
-
   return (
     <div className="h-screen w-screen flex px-40 justify-between">
       <div className="flex flex-col justify-center items-center space-y-16">
