d23eea17d9608adff076638eff7e351d1b60e3ab
Belgutei <z5458085@ad.unsw.edu.au>
2024-01-02 12:53:49 +1100

    changed navbar that active link is highlighted

--- front/app/components/navbar.tsx
+++ front/app/components/navbar.tsx
@@ -2,26 +2,29 @@
 
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const { data: session, status } = useSession();
+  const pathname = usePathname();
+  console.log(pathname);
   if (status === "authenticated") {
     return (
       <nav className="flex justify-between text-text-50 mt-3 w-screen">
         <div className="flex">
-          <Link href="/" className="navbarLink">Problems</Link>
-          <Link href="/analytics" className="navbarLink">Analytics</Link>
+          <Link href="/" className={`${pathname === '/' ? 'navbarLinkAt' : 'navbarLink'}`}>Problems</Link>
+          <Link href="/analytics" className={`${pathname === '/analytics' ? 'navbarLinkAt' : 'navbarLink'}`}>Analytics</Link>
         </div>
         <div className="flex justify-end mr-2">
-          <Link href="/profile" className="navbarLink">{session.user.name}</Link>
+          <Link href="/profile" className={`${pathname === '/profile' ? 'navbarLinkAt' : 'navbarLink'}`}>{session.user.name}</Link>
         </div>
       </nav>
     )
   } else {
     return (
       <div className="flex justify-end text-text-50 mt-3">
-        <Link href="/signin" className="navbarLink">Sign in</Link>
-        <Link href="/signup" className="navbarLink">Sign up</Link>
+        <Link href="/signin" className={`${pathname === '/signin' ? 'navbarLinkAt' : 'navbarLink'}`}>Sign in</Link>
+        <Link href="/signup" className={`${pathname === '/signup' ? 'navbarLinkAt' : 'navbarLink'}`}>Sign up</Link>
       </div>
     )
   }
