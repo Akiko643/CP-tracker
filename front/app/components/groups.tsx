44976a43e6a23ed2e8c59c5e044d17e0cf4e8215
Belgutei <z5458085@ad.unsw.edu.au>
2024-01-19 16:17:47 +1100

    groups-problem page: change text color when hovered

--- front/app/components/groups.tsx
+++ front/app/components/groups.tsx
@@ -7,10 +7,16 @@ export default async function Groups() {
   const groups: Group[] = await getGroups();
   return (
     <section className="w-48 p-4 bg-primary-900 rounded-md self-start">
-      <Link href="/groups" className="text-gray-300">Groups</Link>
+      <Link href="/groups" className="text-gray-300 hover:text-gray-100">
+        Groups
+      </Link>
       <div className="pl-4 flex flex-col">
         {groups.map((group) => (
-          <Link className="text-gray-500" key={group._id} href={`/groups/${group._id}`}>
+          <Link
+            className="text-gray-500 hover:text-gray-300"
+            key={group._id}
+            href={`/groups/${group._id}`}
+          >
             {group.groupName}
           </Link>
         ))}
