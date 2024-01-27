2bf337f0c6cbd25ebe8de41509ccc6ee7b6c879b
Belgutei <z5458085@ad.unsw.edu.au>
2024-01-28 01:51:35 +1100

    refactored the code for problem status

--- front/app/components/Search.tsx
+++ front/app/components/Search.tsx
@@ -16,16 +16,12 @@ function CheckBox({ state, size }: { state: boolean; size: number }) {
 }
 
 export default function Search() {
-  const [todo, setTodo] = useState(false);
-  const [solving, setSolving] = useState(false);
-  const [solved, setSolved] = useState(false);
-  const [skipped, setSkipped] = useState(false);
-
   const searchParams = useSearchParams();
   const pathname = usePathname(); // current pathname /groups/GROUPID
   const { replace } = useRouter();
+  const [status, setStatus] = useState([false, false, false, false]);
+  const statusAll = ["Todo", "Solving", "Solved", "Skipped"];
 
-  // Problem status: Todo / Solving / Solved / Skipped
   useEffect(() => {
     const params = new URLSearchParams(searchParams);
     let statusArray: string[] = [];
@@ -33,31 +29,15 @@ export default function Search() {
       const statusString = decodeURIComponent(params.get("status")!);
       statusArray = statusString.split(",");
     }
-    // in params, checking if Todo is in
-    if (todo && !statusArray.includes("Todo")) {
-      statusArray.push("Todo");
-    } else if (!todo && statusArray.includes("Todo")) {
-      statusArray = statusArray.filter((status) => status !== "Todo");
+    for (let i = 0; i < 4; i++) {
+      if (status[i] && !statusArray.includes(statusAll[i])) {
+        // adding new status to params
+        statusArray.push(statusAll[i]);
+      } else if (!status[i] && statusArray.includes(statusAll[i])) {
+        // removing status from params
+        statusArray = statusArray.filter((status) => status !== statusAll[i]);
+      }
     }
-    // in params, Solving-state
-    if (solving && !statusArray.includes("Solving")) {
-      statusArray.push("Solving");
-    } else if (!solving && statusArray.includes("Solving")) {
-      statusArray = statusArray.filter((status) => status !== "Solving");
-    }
-    // in params, Solved-state
-    if (solved && !statusArray.includes("Solved")) {
-      statusArray.push("Solved");
-    } else if (!solved && statusArray.includes("Solved")) {
-      statusArray = statusArray.filter((status) => status !== "Solved");
-    }
-    // in params, Skipped-state
-    if (skipped && !statusArray.includes("Skipped")) {
-      statusArray.push("Skipped");
-    } else if (!skipped && statusArray.includes("Skipped")) {
-      statusArray = statusArray.filter((status) => status !== "Skipped");
-    }
-
     const statusString = statusArray.join(",");
     const statusEncoded = encodeURIComponent(statusString);
     if (statusEncoded.length > 0) {
@@ -65,63 +45,45 @@ export default function Search() {
     } else {
       replace(`${pathname}`);
     }
-  }, [todo, solving, solved, skipped]);
+  }, [status]);
 
   useEffect(() => {
-    // initial loading -> setting initial values of todo and solved
+    // initial loading -> setting initial values of Status filter
     const params = new URLSearchParams(searchParams);
     if (params.get("status")) {
       const statusString = decodeURIComponent(params.get("status")!);
       const statusArray = statusString.split(",");
-      if (statusArray.includes("Todo")) setTodo(true);
-      else setTodo(false);
-      //
-      if (statusArray.includes("Solving")) setSolving(true);
-      else setSolving(false);
-      //
-      if (statusArray.includes("Solved")) setSolved(true);
-      else setSolved(false);
-      //
-      if (statusArray.includes("Skipped")) setSkipped(true);
-      else setSkipped(false);
+      const initialStatus: boolean[] = [];
+      for (let i = 0; i < 4; i++) {
+        if (statusArray.includes(statusAll[i])) initialStatus.push(true);
+        else initialStatus.push(false);
+      }
+      setStatus(initialStatus);
     }
   }, []);
+
+  function updateStatus(index: number) {
+    setStatus((prevStatus) => {
+      const newStatus = [...prevStatus];
+      newStatus[index] = !newStatus[index];
+      return newStatus;
+    });
+  }
 
   return (
     <div className="w-48 p-4 bg-primary-900 rounded-md mb-2">
       <h3 className="text-gray-300">Status</h3>
       <div className="pl-4">
-        <div
-          className="flex select-none mt-2 items-center"
-          onClick={() => setTodo(!todo)}
-        >
-          <CheckBox state={todo} size={15} />
-          <p className="ml-1 text-gray-500">Todo</p>
-        </div>
-
-        <div
-          className="flex select-none mt-2 items-center"
-          onClick={() => setSolving(!solving)}
-        >
-          <CheckBox state={solving} size={15} />
-          <p className="ml-1 text-gray-500">Solving</p>
-        </div>
-
-        <div
-          className="flex select-none mt-2 items-center"
-          onClick={() => setSolved(!solved)}
-        >
-          <CheckBox state={solved} size={15} />
-          <p className="ml-1 text-gray-500">Solved</p>
-        </div>
-
-        <div
-          className="flex select-none mt-2 items-center"
-          onClick={() => setSkipped(!skipped)}
-        >
-          <CheckBox state={skipped} size={15} />
-          <p className="ml-1 text-gray-500">Skipped</p>
-        </div>
+        {statusAll.map((currStatus, index) => (
+          <div
+            key={index}
+            className="flex select-none mt-2 items-center"
+            onClick={() => updateStatus(index)}
+          >
+            <CheckBox state={status[index]} size={15} />
+            <p className="ml-1 text-gray-500">{currStatus}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
