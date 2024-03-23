3f6f7dfeac6477a9ab4046175c6cccb259ee871f
jangar <jangar551@gmail.com>
2024-03-23 17:24:00 +1100

    removed group api

--- front/api/index.ts
+++ front/api/index.ts
@@ -7,7 +7,7 @@ import { signOut } from "next-auth/react";
 
 const instance = axios.create({
   baseURL: process.env.API_URL,
-  timeout: 10000, 
+  timeout: 10000,
 });
 
 const getToken = async () => {
@@ -167,66 +167,6 @@ export const updateProblem = async (problem: Problem) => {
   }
 };
 
-export const createGroup = async ({ groupName }: { groupName: string }) => {
-  try {
-    const session = await getServerSession(OPTIONS);
-    const { accessToken } = session as any;
-    if (!accessToken) {
-      // TODO: redirect to signin page with error message
-      return;
-    }
-    const token = "Bearer " + accessToken;
-    const { data } = await instance.post(
-      "/groups",
-      {
-        groupName,
-      },
-      {
-        headers: {
-          Authorization: token,
-        },
-      }
-    );
-    return data;
-  } catch (err) {
-    if (axios.isAxiosError(err) && err.response?.status === 401) {
-      return {
-        status: 401,
-      };
-    }
-    // write other error specific code.
-    return;
-  }
-};
-
-export const getGroups = async () => {
-  try {
-    const session = await getServerSession(OPTIONS);
-    const { accessToken } = session as any;
-    if (!accessToken) {
-      // TODO: redirect to signin page with error message
-      return [];
-    }
-
-    const token = "Bearer " + accessToken;
-    const { data } = await instance.get("/groups", {
-      headers: {
-        Authorization: token,
-      },
-    });
-    return data;
-  } catch (err) {
-    if (axios.isAxiosError(err) && err.response?.status === 401) {
-      return {
-        status: 401,
-      };
-    }
-
-    // write other error specific code.
-    return [];
-  }
-};
-
 export const recommendProblem = async ({
   tags,
   rating,
@@ -241,7 +181,7 @@ export const recommendProblem = async ({
       // TODO: redirect to signin page with error message
       return [];
     }
-    
+
     const token = "Bearer " + accessToken;
     const { data } = await instance.get(
       `/recommender?tags=${tags}&rating=${rating}`,
@@ -266,12 +206,3 @@ export const recommendProblem = async ({
     }
   }
 };
-
-export const updateGroup = async () => {};
-export const deleteGroup = async () => {};
-
-export const createProblemToGroup = async () => {};
-export const deleteProblemFromGroup = async () => {};
-
-export const shareGroup = async () => {}; // skip for now
-// CRUD -> CREATE / READ / UPDATE / DELETE
