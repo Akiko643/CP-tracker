1ed932b5178fcfab43c7a0890fff257933cd1b9e
Belgutei <belgutei0323@gmail.com>
2025-03-11 00:29:39 +1100

    removed redirect url in google oauth

--- front/app/signin/page.tsx
+++ front/app/signin/page.tsx
@@ -31,7 +31,7 @@ export default function Signin() {
         <div className="my-5 h-px w-full bg-gray-500"></div>
         <button
           type="button"
-          onClick={() => signIn("google", { callbackUrl: "/" })}
+          onClick={() => signIn("google")}
           className="text-text-50 hover:text-background-900 flex items-center bg-white border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
           <Image
